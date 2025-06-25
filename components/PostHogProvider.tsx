c27428cb628e4b85eb498c9ba115d713924001f1
sagyam <sagyamthapa32@gmail.com>
2025-06-25 20:03:27 +0545

    PostHog changes

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -2,14 +2,17 @@
 
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
     useEffect(() => {
         posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
             api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-            person_profiles: 'always',
+            autocapture: true,
             defaults: '2025-05-24',
+            person_profiles: 'always',
+            capture_performance: true,
+            capture_dead_clicks: true,
         })
     }, [])
 
