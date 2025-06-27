6d6e156fd437fccece760be04191f78b358fa1db
sagyam <sagyamthapa32@gmail.com>
2025-06-27 18:47:38 +0545

    Add proxy for posthog

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -7,7 +7,7 @@ import React, { useEffect } from 'react'
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
     useEffect(() => {
         posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-            api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+            api_host: `/relay-Toward_Frog_Principle5`,
             autocapture: true,
             defaults: '2025-05-24',
             person_profiles: 'always',
--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,25 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
     // Proxy PostHog ingestion and assets requests to the PostHog endpoints
+    // Note: `Toward_Frog_Principle5` is a randomly generated prefix
+    // to bypass Adblockers.
     async rewrites() {
         return [
             {
-                source: '/ingest/static/:path*',
+                source: '/relay-Toward_Frog_Principle5/static/:path*',
                 destination: 'https://us-assets.i.posthog.com/static/:path*',
             },
             {
-                source: '/ingest/:path*',
+                source: '/relay-Toward_Frog_Principle5/:path*',
                 destination: 'https://us.i.posthog.com/:path*',
             },
             {
-                source: '/ingest/decide',
-                destination: 'https://us.i.posthog.com/decide',
+                source: '/relay-Toward_Frog_Principle5/flags',
+                destination: 'https://us.i.posthog.com/flags',
             },
         ]
     },
+    // This is required to support PostHog trailing slash API requests
     skipTrailingSlashRedirect: true,
     allowedDevOrigins: ['http://localhost:3000', 'http://192.168.1.83:3000'],
 }
